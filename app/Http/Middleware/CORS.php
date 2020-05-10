61f5a5594a37a4f16e33b57f42f818bc6301356a
abdelkrimBeghdad <krimo4992@gmail.com>
2020-05-10 15:58:04 +0100

    chngr add CORS3

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -15,8 +15,10 @@ class CORS
      */
     public function handle($request, Closure $next)
     {
-        header('Access-Control-Allow-Origin : *');
-        header('Access-Control-Allow-Headers : Content-type, X-Auth-Token, Authorization, Origin');
-        return $next($request);
+        return $next($request)
+        ->header('Access-Control-Allow-Origin', '*')
+        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+        ->header('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With')
+        ->header('Access-Control-Allow-Credentials',' true');
     }
 }
