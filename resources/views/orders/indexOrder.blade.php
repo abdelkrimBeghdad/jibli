8e3e9d88e7b17c426426e5c5fb1a081dfa195fc0
abdelkrimBeghdad <krimo4992@gmail.com>
2020-10-03 14:24:12 +0100

    nitifiacation 3

--- resources/views/orders/indexOrder.blade.php
+++ resources/views/orders/indexOrder.blade.php
@@ -45,7 +45,7 @@
                         @foreach ($order as $item)
                         <tr>
                             <td scope="row">{{$item->id}}</td>
-                        <td>{{$item->userName->name}} </td>
+                        <td>$item->userName->name </td>
                         <td>{{$item->userName->email}}</td>
                             
                            {{--  <td >
